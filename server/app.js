a006d259d10f47eb144c0fa9e65c1e3d5d622ec2
Guy Altman <guyaltman91@gmail.com>
2023-05-15 18:26:11 +0300

    initialData

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const cors = require("./middlewares/cors");
 const logger = require("./logger/loggerService");
 const connectToDb = require("./DB/dbService");
 const config = require("config");
+const {
+  generateInitialCards,
+  generateInitialUsers,
+} = require("./initialData/initialDataService");
 
 app.use(cors);
 app.use(logger);
@@ -26,4 +30,6 @@ const PORT = config.get("PORT");
 app.listen(PORT, () => {
   console.log(chalk.blueBright(`listening on: http://localhost:${PORT}`));
   connectToDb();
+  generateInitialCards();
+  generateInitialUsers();
 });
