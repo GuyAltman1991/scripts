4e71a0335845d708bd38b9e93448729c4225d494
Guy Altman <guyaltman91@gmail.com>
2023-08-18 19:34:59 +0300

    db

--- server/DB/dataBases/connectToAtlas.js
+++ server/DB/dataBases/connectToAtlas.js
@@ -5,12 +5,11 @@ const config = require("config");
 const NAME = config.get("DB_NAME");
 const PASSWORD = config.get("DB_PASSWORD");
 
-// does not work with variables, but when you put the actual userName and password, its working//
-
 mongoose
   .connect(
     `mongodb+srv://${NAME}:${PASSWORD}@cluster0.fzljkal.mongodb.net/?retryWrites=true&w=majority`
   )
+
   .then(() => console.log(chalk.magentaBright("connected to MongoDB Atlas!")))
   .catch((error) =>
     console.log(chalk.redBright.bold(`could not connect to mongoDb: ${error}`))
